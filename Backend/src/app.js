37cda6b713543bf4a950caf41dea7ec221f8f1fa
Avinash Singh <152694901+avinashsingh03@users.noreply.github.com>
2026-05-10 21:47:31 +0530

    Enabled production CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,9 +4,7 @@ const cors= require("cors");
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
